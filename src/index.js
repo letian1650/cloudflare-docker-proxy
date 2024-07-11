b6ee63fca8b9dc74614133de0a3ab3d77368bac9
letian1650 <1650letian@163.com>
2024-07-11 16:53:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.cswklt.top": dockerHub,
+  "quay.cswklt.top": "https://quay.io",
+  "gcr.cswklt.top": "https://gcr.io",
+  "k8s-gcr.cswklt.top": "https://k8s.gcr.io",
+  "k8s.cswklt.top": "https://registry.k8s.io",
+  "ghcr.cswklt.top": "https://ghcr.io",
+  "cloudsmith.cswklt.top": "https://docker.cloudsmith.io",
+  "ecr.cswklt.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.cswklt.top": dockerHub,
 };
 
 function routeByHosts(host) {
